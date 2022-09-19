d18218019ea0164d1c3f96f6a55c48a97dfb9923
Jolonte <vinicius.nevesco@gmail.com>
2022-09-19 20:18:10 -0300

    :iphone: Responsive

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,112 @@
 <script setup></script>
 
 <template>
-    <header class="flex flex-col items-center p-6 bg-dark">
+    <header
+        class="flex flex-col lg:flex-row 2xl:flex-row items-center p-6 lg:p-0 2xl:p-0 lg:pt-6 2xl:pt-12 lg:px-40 2xl:px-80 bg-dark"
+    >
         <img
-            class="h-5"
+            class="h-5 lg:h-6 2xl:h-11"
             src="src/assets/logo-xbox.png"
             alt="Logo Xbox"
             draggable="false"
         />
-        <nav class="flex w-full mt-7 justify-between">
+        <nav
+            class="flex w-full mt-7 lg:mt-0 2xl:mt-0 justify-between lg:justify-center 2xl:justify-center lg:gap-6 2xl:gap-12"
+        >
             <li
-                class="px-2.5 py-1.5 list-none uppercase text-tyne text-light font-semibold bg-gray bg-opacity-40 rounded-full"
+                class="px-2.5 py-1.5 list-none uppercase text-tyne lg:text-sm 2xl:text-lg text-light font-semibold bg-gray lg:bg-transparent 2xl:bg-transparent bg-opacity-40 rounded-full"
             >
                 Visão geral
             </li>
             <li
-                class="px-2.5 py-1.5 list-none uppercase text-tyne text-light font-semibold bg-gray bg-opacity-40 rounded-full"
+                class="px-2.5 py-1.5 list-none uppercase text-tyne lg:text-sm 2xl:text-lg text-light font-semibold bg-gray lg:bg-transparent 2xl:bg-transparent bg-opacity-40 rounded-full"
             >
                 Jogos
             </li>
             <li
-                class="px-2.5 py-1.5 list-none uppercase text-tyne text-light font-semibold bg-gray bg-opacity-40 rounded-full"
+                class="px-2.5 py-1.5 list-none uppercase text-tyne lg:text-sm 2xl:text-lg text-light font-semibold bg-gray lg:bg-transparent 2xl:bg-transparent bg-opacity-40 rounded-full"
             >
                 Galeria
             </li>
             <li
-                class="px-2.5 py-1.5 list-none uppercase text-tyne text-light font-semibold bg-gray bg-opacity-40 rounded-full"
+                class="px-2.5 py-1.5 list-none uppercase text-tyne lg:text-sm 2xl:text-lg text-light font-semibold bg-gray lg:bg-transparent 2xl:bg-transparent bg-opacity-40 rounded-full"
             >
                 Unbox
             </li>
         </nav>
         <button
-            class="w-full py-3 mt-14 uppercase text-dark font-bold bg-primary"
+            class="w-full 2xl:h-14 lg:w-72 2xl:w-80 py-3 2xl:py-5 2xl:px-7 mt-14 lg:mt-0 2xl:mt-0 uppercase text-dark font-bold bg-primary"
         >
             Ver disponibilidade
         </button>
     </header>
-    <main class="flex flex-col items-center p-6 bg-dark">
-        <section>
-            <div class="flex flex-col items-center gap-3">
-                <h1 class="text-light text-4xl font-semibold">Xbox series X</h1>
-                <h2 class="uppercase text-primary font-bold">
+    <main class="flex flex-col items-center p-6 lg:p-0 2xl:p-0 bg-dark">
+        <section
+            class="lg:flex 2xl:flex lg:justify-center 2xl:justify-center lg:relative 2xl:relative lg:mt-5 2xl:mt-10"
+        >
+            <div
+                class="lg:absolute 2xl:absolute lg:top-16 2xl:top-32 flex flex-col items-center gap-3 lg:gap-6 2xl:gap-12"
+            >
+                <h1
+                    class="text-light text-4xl lg:text-5xl 2xl:text-7xl font-semibold"
+                >
+                    Xbox series X
+                </h1>
+                <h2
+                    class="uppercase text-primary lg:text-xl 2xl:text-2xl font-bold"
+                >
                     pedido antecipado
                 </h2>
             </div>
-            <div class="mt-1">
-                <img src="src/assets/xbox.png" alt="xbox series x" />
+            <div class="relative flex items-end justify-center mt-1">
+                <img
+                    class="absolute"
+                    src="src/assets/xbox.png"
+                    alt="xbox series x"
+                    draggable="false"
+                />
+                <img src="src/assets/luz-verde.png" alt="" draggable="false" />
             </div>
         </section>
-        <section class="flex flex-col items-center mt-14">
-            <h2 class="text-primary text-2xl font-semibold">Desempenho</h2>
-            <h2 class="text-light text-2xl font-semibold">aperfeiçoado</h2>
-            <p class="mt-6 text-center text-grayLight font-medium">
-                O controle sem fio Xbox traz um design elegante, conforto
-                refinado e compartilhamento instantâneo para um favorito comum.
-            </p>
+        <section
+            class="flex flex-col lg:flex-row 2xl:flex-row items-center lg:justify-between 2xl:justify-between mt-14 lg:mt-64 2xl:mt-72 lg:px-40 lg:gap-5 2xl:gap-10"
+        >
+            <div class="flex flex-col w-2/4">
+                <h2
+                    class="text-primary text-2xl lg:text-4xl 2xl:text-6xl font-semibold"
+                >
+                    Desempenho
+                </h2>
+                <h2
+                    class="text-light text-2xl lg:text-4xl 2xl:text-6xl font-semibold"
+                >
+                    aperfeiçoado
+                </h2>
+                <p
+                    class="mt-6 text-center lg:text-left 2xl:text-left text-grayLight lg:text-lg 2xl:text-xl font-medium"
+                >
+                    O controle sem fio Xbox traz um design elegante, conforto
+                    refinado e compartilhamento instantâneo para um favorito
+                    comum.
+                </p>
+            </div>
             <img
-                class="w-52 mt-12"
+                class="w-52 lg:w-80 2xl:w-96 mt-12"
                 src="src/assets/controle-xbox.png"
                 alt="Controle Xbox"
             />
         </section>
-        <section class="flex flex-col mt-24 gap-10">
+        <section
+            class="flex flex-col lg:flex-row 2xl:flex-row mt-24 lg:mt-40 2xl:mt-80 gap-10 lg:gap-0 2xl:gap-0 lg:overflow-x-hidden 2xl:overflow-x-hidden"
+        >
             <img
+                class="visible"
                 src="src/assets/farcry.png"
                 alt="Game: Far Cry 6"
                 draggable="false"
             />
             <img
+                class="visible"
                 src="src/assets/forza.png"
                 alt="Game: Forza Horizon 5"
                 draggable="false"
@@ -79,30 +118,45 @@
                 draggable="false"
             />
         </section>
-        <section class="flex flex-col w-full mt-10">
+        <section
+            class="flex flex-col w-full mt-10 2xl:mt-64 lg:px-40 2xl:px-80"
+        >
             <div class="flex flex-row gap-2">
-                <h2 class="text-primary text-2xl font-semibold">Perguntas</h2>
-                <h2 class="text-light text-2xl font-semibold">frequentes</h2>
+                <h2
+                    class="text-primary text-2xl lg:text-3xl 2xl:text-5xl font-semibold"
+                >
+                    Perguntas
+                </h2>
+                <h2
+                    class="text-light text-2xl lg:text-3xl 2xl:text-5xl font-semibold"
+                >
+                    frequentes
+                </h2>
             </div>
             <!-- common questions -->
-            <div class="flex flex-col mt-5">
-                <!--  -->
+            <div class="flex flex-col mt-5 lg:mt-7 2xl:mt-14">
                 <div class="mt-5 flex flex-col">
                     <div class="flex items-center justify-between">
-                        <p class="text-sm text-light font-medium">
+                        <p
+                            class="text-sm lg:text-lg 2xl:text-2xl text-light font-medium"
+                        >
                             Meus jogos anteriores do Xbox vão funcionar no Xbox
                             Series X?
                         </p>
                         <i class="ri-arrow-up-s-line ri-2x text-primary"></i>
                     </div>
-                    <p class="mt-4 pl-3 text-sm text-grayLight font-medium">
+                    <p
+                        class="mt-4 pl-3 text-sm lg:text-lg 2xl:text-lg text-grayLight font-medium"
+                    >
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     </p>
                     <hr class="mt-5 border border-gray" />
                 </div>
                 <div class="mt-5 flex flex-col">
                     <div class="flex items-center justify-between">
-                        <p class="text-sm text-light font-medium">
+                        <p
+                            class="text-sm lg:text-lg 2xl:text-2xl text-light font-medium"
+                        >
                             O que está incluído no Xbox Series X?
                         </p>
                         <i class="ri-arrow-down-s-line ri-2x text-gray"></i>
@@ -111,7 +165,9 @@
                 </div>
                 <div class="mt-5 flex flex-col">
                     <div class="flex items-center justify-between">
-                        <p class="text-sm text-light font-medium">
+                        <p
+                            class="text-sm lg:text-lg 2xl:text-2xl text-light font-medium"
+                        >
                             Como sei se minha TV é compatível com 4K?
                         </p>
                         <i class="ri-arrow-down-s-line ri-2x text-gray"></i>
@@ -120,16 +176,22 @@
                 </div>
             </div>
         </section>
-        <footer class="flex flex-row w-full mt-12">
+        <footer
+            class="flex flex-row w-full mt-12 lg:mt-24 2xl:mt-48 lg:mb-10 2xl:mb-20 lg:px-40 2xl:px-80"
+        >
             <img
-                class="h-6"
+                class="h-6 2xl:h-11"
                 src="src/assets/logo-xbox.png"
                 alt="Logo Xbox"
                 draggable="false"
             />
-            <div class="flex flex-col w-full items-end">
-                <p class="text-sm text-gray">© Microsoft 2022</p>
-                <p class="text-sm text-gray">Todos os direitos reservados</p>
+            <div
+                class="flex flex-col w-full 2xl:flex-row items-end 2xl:items-center 2xl:justify-end 2xl:gap-14"
+            >
+                <p class="text-sm 2xl:text-lg text-gray">© Microsoft 2022</p>
+                <p class="text-sm 2xl:text-lg text-gray">
+                    Todos os direitos reservados
+                </p>
             </div>
         </footer>
     </main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#9BF00B",
-        // neutral
-        dark: "#0C0C0C",
-        gray: "#777777",
-        grayLight: "#C0C0C0",
-        light: "#FFFFFF",
-      },
-      fontSize: {
-        tyne: "0.6rem",
-      },
-      backgroundImage: {
-        greenLight: "src/assets/luz-verde.png",
-      },
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#9BF00B',
+                // neutral
+                dark: '#0C0C0C',
+                gray: '#777777',
+                grayLight: '#C0C0C0',
+                light: '#FFFFFF'
+            },
+            fontSize: {
+                tyne: '0.6rem'
+            },
+            backgroundImage: {
+                greenLight: 'src/assets/luz-verde.png'
+            }
+        }
     },
-  },
-  plugins: [],
-};
+    plugins: []
+}

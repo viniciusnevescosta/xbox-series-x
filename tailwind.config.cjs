11bf9e216b6a68d215295200514eda93296f028e
Jolonte <vinicius.nevesco@gmail.com>
2022-09-18 21:49:50 -0300

    :sparkles: Header & Footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#9BF00B",
+        // neutral
+        dark: "#0C0C0C",
+        gray: "#777777",
+        grayLight: "#C0C0C0",
+        light: "#FFFFFF",
+      },
+      fontSize: {
+        tyne: "0.6rem",
+      },
+      backgroundImage: {
+        greenLight: "src/assets/luz-verde.png",
+      },
+    },
   },
   plugins: [],
 };
